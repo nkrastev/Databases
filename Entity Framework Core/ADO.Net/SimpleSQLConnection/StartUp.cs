03e30fa0b632a0b8dd64031d112150b86d8046d8
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-08-20 16:07:20 +0300

    Simple SQL Connection

---
+++ Entity Framework Core/ADO.Net/SimpleSQLConnection/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace ConnectionToSql
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection dbCon = new SqlConnection(@"Server=DATA2\MSSQLSERVER01; Database=MinionsDB; Integrated Security=true");
+            dbCon.Open();
+            using (dbCon)
+            {
+                SqlCommand command = new SqlCommand("SELECT COUNT(*) FROM Countries", dbCon);
+                int countriesCount = (int)command.ExecuteScalar();
+                Console.WriteLine("Countries count: {0} ", countriesCount);
+            }
+
+        }
+    }
+}
