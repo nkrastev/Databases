273038a2db2bb88de09cf3eeeaf2adf69299aa01
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-08-23 19:26:47 +0300

    TODO queries

---
+++ Entity Framework Core/ADO.Net/AddMinions/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AddMinion
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            //Minion: <Name> <Age> <TownName>
+            string[] minionInfo = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            //Villain: <Name>
+            string[] villainInfo = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+
+
+
+
+        }
+    }
+}
