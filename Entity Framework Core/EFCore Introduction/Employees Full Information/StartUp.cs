7a78a1f9e9ee864f3a23fc8e86b87206e99fecb2
nkrastev <nikolay.krystev@gmail.com>
2021-08-31 01:02:33 +0300

    Order By

---
+++ Entity Framework Core/EFCore Introduction/Employees Full Information/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using SoftUni.Data;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            using (context)
+            {
+                //Task 3
+                Console.WriteLine(GetEmployeesFullInformation(context));
+            }                   
+
+        }
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees.OrderBy(x => x.EmployeeId).ToList();
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in employees)
+            {
+                sb.AppendLine($"{item.FirstName} {item.LastName} {item.MiddleName} {item.JobTitle} {item.Salary:F2}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+
+    
+}
