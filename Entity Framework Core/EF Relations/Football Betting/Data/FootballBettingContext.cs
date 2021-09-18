821e7e88b9409d9ab00975a2ebab3c311a734258
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-09-18 16:47:24 +0300

    Migrations

---
+++ Entity Framework Core/EF Relations/Football Betting/Data/FootballBettingContext.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P03_FootballBetting.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_FootballBetting.Data
+{
+    public class FootballBettingContext : DbContext
+    {
+        public FootballBettingContext()
+        {
+
+        }
+        public FootballBettingContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Bet> Bets { get; set; }
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<PlayerStatistic> PlayerStatistics { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Town> Towns { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=Football;Integrated Security=true;");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Team>(x =>
+           {
+               x.HasOne(x => x.PrimaryKitColor)
+               .WithMany(x => x.PrimaryKitTeams)
+               .HasForeignKey(x => x.PrimaryKitColorId)
+               .OnDelete(DeleteBehavior.Restrict);
+               
+               x.HasOne(x => x.SecondaryKitColor)
+               .WithMany(x => x.SecondaryKitTeams)
+               .HasForeignKey(x => x.SecondaryKitColorId)
+               .OnDelete(DeleteBehavior.Restrict);
+           });
+
+            modelBuilder.Entity<Game>(x =>
+            {
+                x.HasOne(x => x.HomeTeam).WithMany(x => x.HomeGames).HasForeignKey(x => x.HomeTeamId).OnDelete(DeleteBehavior.Restrict);
+                x.HasOne(x => x.AwayTeam).WithMany(x => x.AwayGames).HasForeignKey(x => x.AwayTeamId).OnDelete(DeleteBehavior.Restrict);                
+            });
+
+            modelBuilder.Entity<PlayerStatistic>(x =>
+            {
+                x.HasKey(x => new { x.PlayerId, x.GameId });
+            });
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+}
---
+++ Entity Framework Core/EF Relations/Football Betting/Data/Models/Color.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_FootballBetting.Data.Models
+{
+    public class Color
+    {
+        public Color()
+        {
+            this.PrimaryKitTeams = new HashSet<Team>();
+            this.SecondaryKitTeams = new HashSet<Team>();
+        }
+        public int ColorId { get; set; }
+
+        public string Name { get; set; }
+
+        //collections
+
+        public ICollection<Team> PrimaryKitTeams { get; set; }
+        public ICollection<Team> SecondaryKitTeams { get; set; }
+    }
+}
