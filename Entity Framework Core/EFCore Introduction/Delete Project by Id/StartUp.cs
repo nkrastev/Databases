72c28cea5c7b256519503cb141db6da42465e4af
nkrastev <nikolay.krystev@gmail.com>
2021-09-28 00:52:59 +0300

    Delete from DB. TODO, check if entities to be deleted are not null

---
+++ Entity Framework Core/EFCore Introduction/Delete Project by Id/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            Console.WriteLine(DeleteProjectById(context));            
+
+        }
+        public static string DeleteProjectById(SoftUniContext context)
+        {            
+            //delete references before removing from project            
+            foreach (var item in context.EmployeesProjects)
+            {
+                if (item.ProjectId==2)
+                {
+                    context.EmployeesProjects.Remove(item);
+                }
+            }
+            context.SaveChanges();
+
+            //Find > gets the entity with specific primary key
+            var project = context.Projects.Find(2);
+            context.Projects.Remove(project);
+            context.SaveChanges();
+
+            var projects = context.Projects.ToList();           
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in projects)
+            {
+                sb.AppendLine($"{item.Name}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+     
+    }
+
+
+}
