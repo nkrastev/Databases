278c231557e209bb1ab7a0a5f506a2ad1513aae2
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-10-05 16:29:18 +0300

    Migrations added

---
+++ Entity Framework Core/Code First/Sales Database/Migrations/20211005130120_ProductsDefaultValueForDescription.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P03_SalesDatabase.Migrations
+{
+    public partial class ProductsDefaultValueForDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                maxLength: 250,
+                nullable: true,
+                defaultValue: "No description",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 250,
+                oldNullable: true,
+                oldDefaultValue: "No description");
+        }
+    }
+}
