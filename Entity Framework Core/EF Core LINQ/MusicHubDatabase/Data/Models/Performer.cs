147b8eb48e04c2513655e30198fcc70f14cdd6de
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-10-05 19:42:35 +0300

    TODO, add calculated prop in Context

---
+++ Entity Framework Core/EF Core LINQ/MusicHubDatabase/Data/Models/Performer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MusicHub.Data.Models
+{
+    public class Performer
+    {
+        public Performer()
+        {
+            this.PerformerSongs = new HashSet<SongPerformer>();
+            //this.Songs = new HashSet<Song>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string LastName { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+
+        [Required]
+        public decimal NetWorth { get; set; }
+
+        public ICollection<SongPerformer> PerformerSongs { get; set; }
+
+        //public ICollection<Song> Songs { get; set; }
+    }
+}
---
+++ Entity Framework Core/EF Core LINQ/MusicHubDatabase/Data/Models/Producer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicHub.Data.Models
+{
+    public class Producer
+    {
+        public Producer()
+        {
+            this.Albums = new HashSet<Album>();
+        }
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+        public string Pseudonym { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public ICollection<Album> Albums { get; set; }
+    }
+}
